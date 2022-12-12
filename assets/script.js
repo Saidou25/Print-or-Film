5e7196f0ecbf38697226e86286dd8b0a0fb77ef1
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2022-12-12 14:19:51 -0500

    test

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,8 @@
-//   var requestUrl = "https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=AIzaSyAwpnTlCB_XLFsQfftXziK3rDq7m9vf6R0";
+var requestUrl = "https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=AIzaSyAwpnTlCB_XLFsQfftXziK3rDq7m9vf6R0";
 var omdb = "http://www.omdbapi.com/?i=tt3896198&apikey=56ad700b";
-//   http://www.omdbapi.com/?apikey=[yourkey]&
+ 
+
+
 
 
 fetch(omdb)
