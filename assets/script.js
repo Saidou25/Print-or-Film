ee84aef01bc0ec1f5561e63d256159bf49fd5d54
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2022-12-14 11:24:57 -0500

    linked all buttons to search results

--- assets/script.js
+++ assets/script.js
@@ -2,141 +2,149 @@ var googleBooks = "https://www.googleapis.com/books/v1/volumes?q=flowers+inautho
 var omdb = "http://www.omdbapi.com/?i=tt3896198&apikey=56ad700b";
 //   http://www.omdbapi.com/?apikey=[yourkey]&
 
+
+
+
 var books = $("#books");
 
 // Search Bar
 $(".btn-primary").click(function (event) {
-    event.preventDefault();
-    var input = $("#textInput").val();
-    var bookUrl = 'https://www.googleapis.com/books/v1/volumes?q=' + input;
-    var movieURL = 'http://www.omdbapi.com/?apikey=56ad700b&t=' + input;
+  event.preventDefault();
+  var input = $("#textInput").val();
+  var bookUrl = 'https://www.googleapis.com/books/v1/volumes?q=' + input;
+  var movieURL = 'http://www.omdbapi.com/?apikey=56ad700b&t=' + input;
 
-    // Book Results
-    fetch(bookUrl)
-        .then(function (response) {
-            if (response.ok) {
-                console.log(response);
-                response.json().then(function (data) {
-                    console.log(data);
-                    $("#card-container").empty();
-                    data.items.forEach(item => item.volumeInfo.averageRating ? true : item.volumeInfo.averageRating = 0);
-                    var items = data.items;
-                    items.sort((a, b) => b.volumeInfo.averageRating - a.volumeInfo.averageRating);
-                    for (let i = 0; i < data.items.length; i++) {
-                        var title = (data.items[i].volumeInfo.title);
-                        var rating = (data.items[i].volumeInfo.averageRating);
-                        var img = (data.items[i].volumeInfo.imageLinks.smallThumbnail);
+  if (location.href.includes("index.html") || location.href.includes("watchlist.html") ) {
 
-                        var col = $('<div>').addClass('col-12 col-lg-2 col-md-3 col-sm-4 col-xs-12');
-                        var card = $('<div>').addClass('card');
-                        var cardImg = $('<img>').addClass('card-img-top');
-                        var cardBody = $('<div>').addClass('card-body');
-                        var cardTitle = $('<h5>').addClass('card-title');
-                        var cardParagraph = $('<p>').addClass('card-text');
-                        var cardLink = $('<button>').addClass('btn btn-info');
+    location.assign("./search-results.html")
+  };
 
-                        cardTitle.text(title);
-                        cardParagraph.text(rating + "/5");
-                        cardImg.attr('src', img);
+  // Book Results
+  fetch(bookUrl)
+    .then(function (response) {
+      if (response.ok) {
+        console.log(response);
+        response.json().then(function (data) {
+          console.log(data);
+          $("#card-container").empty();
+          data.items.forEach(item => item.volumeInfo.averageRating ? true : item.volumeInfo.averageRating = 0);
+          var items = data.items;
+          items.sort((a, b) => b.volumeInfo.averageRating - a.volumeInfo.averageRating);
+          for (let i = 0; i < data.items.length; i++) {
+            var title = (data.items[i].volumeInfo.title);
+            var rating = (data.items[i].volumeInfo.averageRating);
+            var img = (data.items[i].volumeInfo.imageLinks.smallThumbnail);
 
-                        cardBody
-                            .append(cardTitle)
-                            .append(cardParagraph)
-                            .append(cardLink);
+            var col = $('<div>').addClass('col-12 col-lg-2 col-md-3 col-sm-4 col-xs-12');
+            var card = $('<div>').addClass('card');
+            var cardImg = $('<img>').addClass('card-img-top');
+            var cardBody = $('<div>').addClass('card-body');
+            var cardTitle = $('<h5>').addClass('card-title');
+            var cardParagraph = $('<p>').addClass('card-text');
+            var cardLink = $('<button>').addClass('btn btn-info');
 
-                        card
-                            .append(cardImg)
-                            .append(cardBody);
+            cardTitle.text(title);
+            cardParagraph.text(rating + "/5");
+            cardImg.attr('src', img);
 
-                        col
-                            .append(card);
+            cardBody
+              .append(cardTitle)
+              .append(cardParagraph)
+              .append(cardLink);
+
+            card
+              .append(cardImg)
+              .append(cardBody);
+
+            col
+              .append(card);
 
 
-                        $("#card-container").append(col);
-                        // generateCard(data.items[i].volumeInfo);
-                        
-                        // Local Storage
-                    var saveBookBtn = $('.btn-info');
+            $("#card-container").append(col);
+            // generateCard(data.items[i].volumeInfo);
 
-                    $(saveBookBtn).click(function () {
-                     console.log("book click");
-                    })
-                    }
-                });
-            } else {
-                alert('Error: ' + response.statusText);
-                // No alerts - redirect to error message
-            }
-        })
-        .catch(function (error) {
-            // No alerts - redirect to error message
-            alert('No results found!');
+            // Local Storage
+            var saveBookBtn = $('.btn-info');
+
+            $(saveBookBtn).click(function () {
+              console.log("book click");
+            })
+          }
         });
+      } else {
+        alert('Error: ' + response.statusText);
+        // No alerts - redirect to error message
+      }
+    })
+    .catch(function (error) {
+      // No alerts - redirect to error message
+      alert('No results found!');
+    });
 
-    // Movie Results
-    fetch(movieURL)
-        .then(function (response) {
-            if (response.ok) {
-                console.log(response);
-                response.json().then(function (data) {
-                    $("#movie-container").empty();
-                    console.log(data);
-                    var movieTitle = (data.Title);
-                    var movieRating = (data.imdbRating);
-                    console.log(movieRating);
-                    console.log(movieTitle);
+  // Movie Results
+  fetch(movieURL)
+    .then(function (response) {
+      if (response.ok) {
+        console.log(response);
+        response.json().then(function (data) {
+          $("#movie-container").empty();
+          console.log(data);
+          var movieTitle = (data.Title);
+          var movieRating = (data.imdbRating);
+          console.log(movieRating);
+          console.log(movieTitle);
 
-                    var movieCard = $('<div>').addClass('card');
-                    var movieCardImg = $('<img>').addClass('card-img-top');
-                    var movieCardBody = $('<div>').addClass('card-body');
-                    var movieCardTitle = $('<h5>').addClass('card-title');
-                    var movieCardParagraph = $('<p>').addClass('card-text');
-                    var movieCardLink = $('<button>').addClass('btn btn-secondary');
+          var movieCard = $('<div>').addClass('card');
+          var movieCardImg = $('<img>').addClass('card-img-top');
+          var movieCardBody = $('<div>').addClass('card-body');
+          var movieCardTitle = $('<h5>').addClass('card-title');
+          var movieCardParagraph = $('<p>').addClass('card-text');
+          var movieCardLink = $('<button>').addClass('btn btn-secondary');
 
-                    movieCardBody
-                        .append(movieCardTitle)
-                        .append(movieCardParagraph)
-                        .append(movieCardLink);
+          movieCardBody
+            .append(movieCardTitle)
+            .append(movieCardParagraph)
+            .append(movieCardLink);
 
-                    movieCard
-                        .append(movieCardImg)
-                        .append(movieCardBody);
+          movieCard
+            .append(movieCardImg)
+            .append(movieCardBody);
 
-                    movieCardTitle.text(movieTitle);
-                    movieCardParagraph.text(movieRating + "/10");
-                    console.log(movieCardParagraph);
+          movieCardTitle.text(movieTitle);
+          movieCardParagraph.text(movieRating + "/10");
+          console.log(movieCardParagraph);
 
-                    $("#movie-container").append(movieCard);
-                    // generateCard(data.items[i].volumeInfo);
+          $("#movie-container").append(movieCard);
+          // generateCard(data.items[i].volumeInfo);
 
-                    // Local Storage
-                    var saveMovieBtn = $('.btn-secondary');
+          // Local Storage
+          var saveMovieBtn = $('.btn-secondary');
 
-                    $(saveMovieBtn).click(function () {
-                     console.log("click");
-                    })
-                }
-                );
-            } else {
-                // no alert messages
-                alert('Error: ' + response.statusText);
-            }
-        })
-        .catch(function (error) {
-            // no alert messages
-            alert('No results found!');
-        });
+          $(saveMovieBtn).click(function () {
+            console.log("click");
+          })
+        }
+        );
+      } else {
+        // no alert messages
+        alert('Error: ' + response.statusText);
+      }
+    })
+    .catch(function (error) {
+      // no alert messages
+      alert('No results found!');
+    });
 
-// eb-005
+  // eb-005
 })
 
 // Local Storage
 var saveBtn = $('.btn-secondary');
 
 $(saveBtn).click(function () {
-    var text = $(this).siblings(cardImg).val();
-    console.log("click");
-    localStorage.setItem(time, text);
+  var text = $(this).siblings(cardImg).val();
+  console.log("click");
+  localStorage.setItem(time, text);
 })
 
 
@@ -158,6 +166,5 @@ $(saveBtn).click(function () {
 // };
 
 // searchButton = addEventListener('click', goSearch);
-=======
-})
+
 // main
